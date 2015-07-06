cdf6e3c7399dca783320a8cb4a4b5579138bd12d
Peter Elmers <peter.elmers@yahoo.com>
2015-07-05 21:19:07 -0700

    Do not touch empty hrefs, clarify options. 👟🔮
    When we rerwite empty hrefs, the behavior of the anchor changes:
    clicking it will reload the page, rather than do nothing, which breaks
    some web sites (DXR itself, ironically enough).

--- data/link-rewriter.js
+++ data/link-rewriter.js
@@ -3,11 +3,7 @@ var tag = "moz-mxr-to-dxr";
 self.port.on(tag, function(url) {
     var anchors = document.getElementsByTagName("a");
     for (var i = 0; i < anchors.length; i++) {
-        anchors[i].href = anchors[i].href.replace("mxr.mozilla.org", url);
-        // Workaround pending fix of bug 1178103:
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=1178103
-        if (anchors[i].href.endsWith("/")) {
-            anchors[i].href = anchors[i].href.slice(0, -1);
-        }
+        if (anchors[i].href)
+            anchors[i].href = anchors[i].href.replace("mxr.mozilla.org", url);
     }
 });
