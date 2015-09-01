d3e7ed1874e870e6f85a27bd3d34a0dc8c0cc635
Peter Elmers <peter.elmers@yahoo.com>
2015-09-01 17:51:21 -0500

    Explicitly test that a URL contains mxr.mozilla.org 😎👊🏼

--- data/link-rewriter.js
+++ data/link-rewriter.js
@@ -3,7 +3,7 @@ var tag = "moz-mxr-to-dxr";
 self.port.on(tag, function(url) {
     var anchors = document.getElementsByTagName("a");
     for (var i = 0; i < anchors.length; i++) {
-        if (anchors[i].href)
+        if (anchors[i].href.indexOf("mxr.mozilla.org") !== -1)
             anchors[i].href = anchors[i].href.replace("mxr.mozilla.org", url);
     }
 });
